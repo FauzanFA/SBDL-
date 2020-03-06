84971b60d5ca6c5699b19049ea1f3d37d86f9622
saepulmahri <50070719+saepulmahri@users.noreply.github.com>
2020-03-06 15:15:39 +0700

    Add files via upload

---
+++ SBDL/TUGAS 1/data_user.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Inang: 127.0.0.1
+-- Waktu pembuatan: 06 Mar 2020 pada 09.02
+-- Versi Server: 5.5.27
+-- Versi PHP: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Basis data: `data_keluar_masuk_bahan_baku_gudang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_user`
+--
+
+CREATE TABLE IF NOT EXISTS `data_user` (
+  `Username` varchar(20) NOT NULL,
+  `Password` varchar(40) NOT NULL,
+  `Login_Hash` varchar(30) NOT NULL,
+  PRIMARY KEY (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
